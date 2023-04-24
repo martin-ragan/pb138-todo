ad1f1661bd51224be80e183a91f938ed92c94e92
Martin Ragan <xragan@fi.muni.cz>
2023-04-25 00:07:23 +0200

    fix(task-detail)!: using selector to get the task

--- src/routes/TaskDetail.tsx
+++ src/routes/TaskDetail.tsx
@@ -1,21 +1,16 @@
 import { useParams } from 'react-router-dom';
-import fetcher from '../models/fetcher';
-import { Todo } from '../models/models';
-import useSWR from 'swr'
+import { useAppSelector } from '../app/hooks';
 
 export const TaskDetail = () => {
     const { id } = useParams<{ id: string }>();
-    const { data: todo, error} = useSWR<Todo>(`http://localhost:4000/tasks/${id}`, fetcher);
-
-    if (error) return <div>failed to load</div>;
-    if (!todo) return <div>loading...</div>;
-
+    const todo = useAppSelector(state => state.tasks.tasks.find(task => task.id === id));
+    
     return (
         <>
             <div className="bg-white w-[25rem] rounded px-6 py-4 flex flex-col items-start gap-8 drop-shadow-sm">
-                <h1 className='text-2xl font-bold tracking-wide'>{todo.title}</h1>
-                <h3 className='text-xl'><b>Assignee:</b> {todo.assignee}</h3>
-                <p>{todo.description}</p>
+                <h1 className='text-2xl font-bold tracking-wide'>{todo?.title}</h1>
+                <h3 className='text-xl'><b>Assignee:</b> {todo?.assignee}</h3>
+                <p>{todo?.description}</p>
             </div>
         </>
     )
