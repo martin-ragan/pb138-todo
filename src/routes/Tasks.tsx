2897434761e9456d7d8aad0aee3c52aa4fafc036
Martin Ragan <xragan@fi.muni.cz>
2023-04-25 00:06:31 +0200

    fix(tasks): using dispatch from redux

--- src/routes/Tasks.tsx
+++ src/routes/Tasks.tsx
@@ -1,42 +1,26 @@
 import { TodoList } from '../components/TodoList/TodoList'
-import axios from 'axios';
 import { Todo } from '../models/models';
-import fetcher from '../models/fetcher';
-import useSWR from 'swr';
 import { AddForm } from '../components/AddForm/AddForm';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { addTask, completeTask, deleteTask } from '../features/tasks/tasks-slice';
 
 export const Tasks = () => {
-    const { data: todoData, error, mutate } = useSWR('http://localhost:4000/tasks', fetcher);
 
-    if (error) return <div>failed to load</div>;
-    if (!todoData) return <div>loading...</div>;
-
+    const todoData = useAppSelector(state => state.tasks.tasks);
+    const dispatch = useAppDispatch();
+    
     const deleteTodo = async (id: string) => {
-        mutate(async (todos: Todo[]) => {
-            await axios.delete(`http://localhost:4000/tasks/${id}`);
-
-            const newTodos = todos.filter(todo => todo.id !== id)
-
-            return newTodos;
-        });
+        dispatch(deleteTask(id));
     }
 
     const addTodo = (data: Omit<Todo, "id" | "completed">) => {
-        mutate(async (todos: Todo[]) => {
-            const newTask = await axios.post('http://localhost:4000/tasks', {
-                data
-            });
-
-            return [...todos, newTask.data];
-        });
+        dispatch(addTask(data));
     }
 
     const changeComplete = (id: string) => {
-        mutate(async (todos: Todo[]) => {
-            await axios.post(`http://localhost:4000/tasks/${id}/complete`);
-            return todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo)
-        });
+        dispatch(completeTask(id));
     }
+
     return (
         <>
             {todoData.length > 0 && <TodoList
